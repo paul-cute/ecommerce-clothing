507c57cf46325aab404b31d3b508d3528d16d272
paul-cute <paulcuteflorin@protonmail.com>
2023-04-25 00:23:12 +0300

    form component

--- src/routes/signin/Signin.jsx
+++ src/routes/signin/Signin.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import {createUserDocumentFromAuth, signInWithGooglePopup} from '../../utils/firebase/firebase'
+import {createUserDocumentFromAuth, signInWithGooglePopup, signInWithGoogleRedirect} from '../../utils/firebase/firebase'
+import SignUpForm from '../../components/signUp/SignUpForm';
 
 function Signin() {
 
-    const logGoogleUser = async() => {
-        const {user} = await signInWithGooglePopup();
-        console.log(user)
-        const userDocRef = await createUserDocumentFromAuth(user)
-     
-    }
+  const logGoogleUser = async() => {
+      const {user} = await signInWithGooglePopup();
+      const userDocRef = await createUserDocumentFromAuth(user)
+  }
+
   return (
     <div>
         <h1>Signin Page</h1>
         <button onClick={logGoogleUser}>singIn</button>
+        <SignUpForm/>
     </div>
   )
 }
