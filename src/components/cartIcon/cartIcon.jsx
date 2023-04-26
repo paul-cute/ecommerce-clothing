6b4f8cb2b7fa712d7847170d526ea1c1e2b4e721
paul-cute <paulcuteflorin@protonmail.com>
2023-04-26 22:51:05 +0300

    shop components

--- src/components/cartIcon/cartIcon.jsx
+++ src/components/cartIcon/cartIcon.jsx
@@ -13,6 +13,7 @@ const CartIcon = () => {
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon'/>
             <span className='item-count'>0</span>
+            
         </div>
     )
 }
