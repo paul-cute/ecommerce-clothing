a8e1b80ea07eb5e8a3cc831537339ae5dc652bd5
paul-cute <paulcuteflorin@protonmail.com>
2023-04-29 21:11:55 +0300

    project finish

--- src/components/directoryItem/directoryItem.jsx
+++ src/components/directoryItem/directoryItem.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import './directoryItem.scss'
+import { useNavigate } from 'react-router-dom';
+
 
 const DirectoryItem = ({category}) => {
-  const {imageUrl, title} = category;
+  const {imageUrl, title, route} = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route)
   return (
-    <div className='directory-item-container'>
+    <div className='directory-item-container' onClick={onNavigateHandler}>
       <div
         className='background-image'
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className='body'>
+      <div className='body' >
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
